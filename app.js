bcdc6a42d478d78f24cb5ff73b196f97059e785a
Beth Wayman <beth.wayman01@gmail.com>
2023-03-29 17:46:39 -0400

    updated errors

--- app.js
+++ app.js
@@ -19,23 +19,23 @@ const highlightMenu = () => {
     let scrollPos = window.scrollY;
 
     // adds 'highlight' class to my menu items
-    if(window.innerWidth > 960 && scrollPos < 600) {
+    if (window.innerWidth > 960 && scrollPos < 600) {
         homeMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         return;
-    } else if(window.innerWidth > 960 && scrollPos < 1400) {
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
         aboutMenu.classList.add('highlight');
         homeMenu.classList.remove('highlight');
         servicesMenu.classList.remove('highlight');
         return;
 
-    } else if(window.innerWidth > 960 && scrollPos < 2345) {
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
         servicesMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         return;
     }
 
-    if((elm && window.innerWidth < 960 && scrollPos < 600) || elm) {
+    if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
         elem.classList.remove('highlight');
     }
 };
